d550dc6d34209c4299bad427a99fa2f5f1210dc5
Michée Allidjinou <charaznable@site3.local>
2023-02-04 17:55:47 +0100

    feat: make content width responsive
    Make content width full on large screens and under.
    Make content width max to 3/4 of screen width on
    larger screens.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,9 @@
 	<meta name="description" content="Made with Sveltekit 1.0" />
 </svelte:head>
 
-<div class="flex flex-col min-h-full">
-	<Navbar />
-	<slot />
+<div class="flex flex-col items-center min-h-screen bg-slate-300">
+	<div class="flex flex-col min-h-screen lg:w-3/4 w-screen bg-green-200 px-8">
+		<Navbar />
+		<slot />
+	</div>
 </div>
