4255522f302ed20f7e8a1efb420905e7ff3547f8
Michée Allidjinou <charaznable@site3.local>
2023-02-04 17:57:15 +0100

    feat: remove horizontal padding

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,3 +1,8 @@
-<nav>
+<nav class="flex justify-between py-8">
 	<a href="/">michee allidjinou</a>
+	<ul class="flex gap-4">
+		<li><a href="/resume">resume</a></li>
+		<li><a href="/blog">blog</a></li>
+		<li><a href="/about">about</a></li>
+	</ul>
 </nav>
